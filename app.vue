7d4d1ae1632b0c576f84fc7d6c4b82318cd26d90
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2020-05-23 15:31:56 +0300

    working minesweeper

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <MineSweeper></MineSweeper>
+</template>
+
+<script lang="ts">
+import MineSweeper from './components/mine-sweeper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MineSweeper
+  }
+}
+</script>
---
+++ main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './app.vue'
+
+import './assets/css/normalize.css';
+import './assets/css/reset.css';
+import './assets/css/global.css';
+import './assets/css/index.css'
+
+createApp(App).mount('#app')
